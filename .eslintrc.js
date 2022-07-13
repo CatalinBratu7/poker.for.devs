4979039c788ee9d1d92cf30487e23ed625dac5c7
Catalin Bratu <bratu.acb@gmail.com>
2022-07-13 16:58:09 +0300

    add vue-router

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   plugins: ["vue", "prettier"],
   rules: {
+    "prettier/prettier": "error",
     "max-len": ["error", { code: 120, ignoreUrls: true }],
   },
 };
